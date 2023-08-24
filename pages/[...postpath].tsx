bdaf0a94c09809ba7103b5e13f3d34abc064109f
kashiframzan651 <142730838+kashiframzan651@users.noreply.github.com>
2023-08-24 19:48:01 +0500

    Update [...kkkkk

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+kkkimport React from 'react';
 import Head from 'next/head';
 import { GetServerSideProps } from 'next';
 import { GraphQLClient, gql } from 'graphql-request';
